415c25a0baccebb769145491552229587abf3ffe
Rahmat Ramadhan <75988718+pinoskuno@users.noreply.github.com>
2023-05-31 20:43:44 +0700

    Delete create_table.sql

--- create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE records (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    title VARCHAR(25) NOT NULL,
-    attachment VARCHAR(255),
-    date DATETIME NOT NULL,
-    article TEXT
-);
