ac777c49d22139dca11defd16066ae93cded9cb8
Rahmat Ramadhan <rakarr15@gmail.com>
2023-06-14 09:19:08 +0700

    Input Article dan Informasi
    input menggunakan python untuk ke firestore

---
+++ Informasi/app.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+# Inisialisasi Firebase Admin SDK
+cred = credentials.Certificate('harvestia-5aadc-firebase-adminsdk-aua1d-4ff4dd810c.json')
+firebase_admin.initialize_app(cred)
+
+# Akses Firestore
+db = firestore.client()
+
+# Nama koleksi Firestore
+collection_name = 'informasi'
+
+# Baca data dari file JSON
+with open('data.json') as f:
+    data = json.load(f)
+
+# Unggah data ke Firestore
+for doc_id, doc_data in data.items():
+    doc_ref = db.collection(collection_name).document(doc_id)
+    doc_ref.set(doc_data)
+
+print("Data berhasil diunggah ke Firestore.")
